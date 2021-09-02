84224a7b986551cff6d42782c1bb1ceb3b7322d6
LearnGitHub <thanhnd311020@gmail.com>
2021-09-02 09:04:13 +0700

    Add project

--- src/main/java/edu/poly/config/InterceptorConfig.java
+++ src/main/java/edu/poly/config/InterceptorConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import edu.poly.entity.Category;
+
 import edu.poly.interceptor.GlobalInterceptor;
-import edu.poly.reponsitory.CategoryReponsitory;
+
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer{
